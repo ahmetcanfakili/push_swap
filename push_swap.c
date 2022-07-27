89e9fe6c7e63a475a1db9790436f3012260bfc93
ahmetcanfakili <ahmetcanfakili50@gmail.com>
2022-07-27 13:58:33 +0300

    working on it

---
+++ push_swap.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afakili <ahmetcanfakili50@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 13:45:42 by afakili           #+#    #+#             */
+/*   Updated: 2022/07/27 13:56:27 by afakili          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int main()
+{
+
+}
+
+void swap_a()
+{
+	
+}
+
+void swap_b()
+{
+	
+}
+
+void sa_and_sb()
+{
+	
+}
+
+void push_a()
+{
+	
+}
+
+void push_b()
+{
+	
+}
+
+void rotate_a()
+{
+	
+}
+
+void rotate_b()
+{
+	
+}
+
+void ra_and_rb()
+{
+	
+}
+
+void reverse_rotate_a()
+{
+	
+}
+
+void reverse_rotate_b()
+{
+	
+}
+
+void rra_and_rrb()
+{
+	
+}
